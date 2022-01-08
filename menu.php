dfc6bc08137feb482b4b0d02af8a8b0afb009e2b
Sebastian Vanegas <93490015+BigJack2@users.noreply.github.com>
2022-01-07 19:25:37 -0500

    Add files via upload

---
+++ menu.php
@@ -0,0 +1,124 @@
+<div class="col-sm-12 col-lg-3 col-xl-3 col-xxl-2 overflow-auto">
+  <div id="menu-content" class="flex-shrink-3 sticky-top bg-white overflow-auto bg-dark">
+    <a href="index.html" class="d-flex align-items-center py-3 mb-3 link-dark text-decoration-none border-bottom">
+      <img width="35px" style="margin-left: 14px" src="img/icons/laravel.svg" />
+      <img width="35px" style="margin-left: 10px" src="img/icons/vue.svg" />
+      <img width="35px" style="margin-left: 10px" src="img/icons/bootstrap.svg" />
+      <img width="35px" style="margin-left: 10px" src="img/icons/visual-studio-code.svg" />
+      <img width="35px" style="margin-left: 10px" src="img/icons/sass.svg" />
+    </a>
+
+    <ul class="list-unstyled ps-0">
+      <li class="mb-1">
+        <button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#home-collapse" aria-expanded="true">
+          Comandos
+        </button>
+        <div class="collapse" id="home-collapse">
+          <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+            <li><a href="index.php#laravel" class="link-dark rounded">Comandos Laravel</a></li>
+            <li><a href="index.php#vue" class="link-dark rounded">Comandos Vue</a></li>
+            <li><a href="index.php#vsc" class="link-dark rounded">Atajos Visual Studio Code</a></li>
+            <li>
+              <a href="https://sass-lang.com/guide" class="link-dark rounded" target="_blank">Comandos Sass Css <i class="bx bx-exit"></i></a>
+            </li>
+            <li>
+              <a href="https://bluuweb.github.io/desarrollo-web-bluuweb/07-01-git/#enlaces" class="link-dark rounded" target="_blank">Comandos Git Bluuweb <i class="bx bx-exit"></i></a>
+            </li>
+            <li>
+              <a href="https://bluuweb.github.io/desarrollo-web-bluuweb/07-00-terminal/" class="link-dark rounded" target="_blank">Comandos CMD Bluuweb <i class="bx bx-exit"></i></a>
+            </li>
+          </ul>
+        </div>
+      </li>
+      <li class="mb-1">
+        <button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#notas-collapse" aria-expanded="false">
+          Notas
+        </button>
+        <div class="collapse" id="notas-collapse">
+          <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+            <li><a href="#" class="link-dark rounded">Laravel</a></li>
+            <li><a href="#" class="link-dark rounded">Vuejs</a></li>
+          </ul>
+        </div>
+      </li>
+      <li class="mb-1">
+        <button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#css-collapse" aria-expanded="false">
+          Css
+        </button>
+        <div class="collapse" id="css-collapse">
+          <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+            <li><a href="css.php#css" class="link-dark rounded">Clases genericas</a></li>
+            <li><a href="css.php#tabla" class="link-dark rounded">Tabla</a></li>
+            <li><a href="css.php#fantasma" class="link-dark rounded">Boton fantasma</a></li>
+            <li><a href="css.php#link" class="link-dark rounded">Boton Link</a></li>
+            <li><a href="css.php#paginador" class="link-dark rounded">Boton paginador</a></li>
+            <li><a href="css.php#file" class="link-dark rounded">Boton File</a></li>
+            <li><a href="css.php#menu" class="link-dark rounded">Menú desplegable</a></li>
+            <li><a href="css.php#tooltip" class="link-dark rounded">Tooltips</a></li>
+          </ul>
+        </div>
+      </li>
+      <li class="mb-1">
+        <button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#instal-collapse" aria-expanded="false">
+          Instaladores
+        </button>
+        <div class="collapse" id="instal-collapse">
+          <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+            <li><a href="instaladores.php#instaladores" class="link-dark rounded">Xampp</a></li>
+            <li><a href="instaladores.php#vue2" class="link-dark rounded">Vuejs</a></li>
+            <li><a href="instaladores.php#laravel2" class="link-dark rounded">Laravel y Composer</a></li>
+            <li><a href="instaladores.php#vscode" class="link-dark rounded">Configuración VSC</a></li>
+            <li>
+              <a href="https://rubyinstaller.org/downloads/" class="link-dark rounded" target="_blank">Ruby Instaler para Sass Css <i class="bx bx-exit"></i></a>
+            </li>
+          </ul>
+        </div>
+      </li>
+
+      <li class="border-top my-3"></li>
+
+      <li class="mb-1">
+        <button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#drive-collapse" aria-expanded="false">
+          Material de apoyo
+        </button>
+        <div class="collapse" id="drive-collapse">
+          <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+            <li>
+              <a href="material_de_apoyo.php#tutoriales" class="link-dark rounded">Tutoriales</a>
+            </li>
+            <li>
+              <a href="material_de_apoyo.php#marcadores" class="link-dark rounded">Marcadores</a>
+            </li>
+            <li>
+              <a href="https://drive.google.com/drive/folders/0B1R6zq7kCgNAcW03ajhCQUpnSmM?resourcekey=0-gs5HhVQFzYaZKYItQBcmlg&usp=sharing" target="_blank" class="link-dark rounded">Plantillas Drive <i class="bx bx-exit"></i></a>
+            </li>
+            <li>
+              <a href="https://drive.google.com/drive/folders/1NTtfJJFlc6S6LFbxwE94ay2JyafMUkeG?usp=sharing" target="_blank" class="link-dark rounded">Programas Drive <i class="bx bx-exit"></i></a>
+            </li>
+            <li>
+              <a href="https://drive.google.com/drive/folders/0B1R6zq7kCgNAQU5PUUV0R2hrVDg?resourcekey=0-lB6JlQEoZtPX8F8yy6Ov3g&usp=sharing" target="_blank" class="link-dark rounded">Corel Draw Drive <i class="bx bx-exit"></i></a>
+            </li>
+          </ul>
+        </div>
+      </li>
+
+      <li class="border-top my-3"></li>
+
+      <li class="mb-1">
+        <button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#github-collapse" aria-expanded="false">
+          Github Pages
+        </button>
+        <div class="collapse" id="github-collapse">
+          <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+            <li>
+              <a href="https://bigjack2.github.io/pruebaboostrap5.github.io/" target="_blank" class="link-dark rounded">Demo Bootstrap 5 <i class="bx bx-exit"></i></a>
+            </li>
+            <li>
+              <a href="https://bigjack2.github.io/portafolio_vanegas.github.io/" target="_blank" class="link-dark rounded">Portafolio web <i class="bx bx-exit"></i></a>
+            </li>
+          </ul>
+        </div>
+      </li>
+    </ul>
+  </div>
+</div>
---
+++ plantilla.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <meta name="description" content="" />
+
+  <title>Guia Jhon Vanegas</title>
+  <link rel="shortcut icon" href="img/icons/laravel.ico" />
+
+  <!-- Bootstrap core CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+  <!--Fuentes ubuntu de google fonts-->
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link href="https://fonts.googleapis.com/css2?family=Source+Code+Pro:wght@400;500&display=swap" rel="stylesheet" />
+  <link href="https://fonts.googleapis.com/css2?family=Ubuntu&display=swap" rel="stylesheet" />
+
+  <!-- Custom styles for this template -->
+  <link href="css/sidebars.css" rel="stylesheet" />
+
+  <!-- Custom styles for this icons -->
+  <link href="https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css" rel="stylesheet" />
+
+  <!-- Custom styles for this template -->
+  <link href="css/estilos.css" rel="stylesheet" />
+
+  <style></style>
+</head>
+
+<body>
+  <div class="container-fluid">
+    <div class="row">
+
+      <?php include 'menu.php'; ?>
+
+      <div class="col-sm-12 col-lg-9 col-xl-9 col-xxl-10 pt-2">
+
+      
+
+      </div>
+    </div>
+  </div>
+</body>
+
+</html>
